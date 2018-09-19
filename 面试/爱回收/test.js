8202fa29970ecf94c8d22eb47fe030ea2b22ef21
cxm <18255117203@163.com>
2018-09-19 22:52:42 +0800

    add0919

---
+++ 面试/爱回收/test.js
@@ -0,0 +1,21 @@
+class A {
+    constructor() {
+        this.a = 1;
+    }
+    Afun(){
+
+    }
+}
+
+class B extends A {
+    constructor() {
+        super();
+        this.b = 2;
+    }
+    Bfun(){
+
+    }
+}
+
+var b = new B();
+console.log(b);

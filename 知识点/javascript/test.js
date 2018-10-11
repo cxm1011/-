d55a9cb0f975819c8e23ec5b1fe7a896c9781201
cxm <18255117203@163.com>
2018-10-11 23:00:47 +0800

    add1011

--- 知识点/javascript/test.js
+++ 知识点/javascript/test.js
@@ -6,3 +6,8 @@ console.log(arr.length);  // 4
 // 给数组添加属性，属性名是个数字字符串，它会变为数字下标，同时改变数组的长度
 arr['4'] = 'arr4';
 console.log(arr.length);   // 5
+
+var arr = ['a','b'];
+for(var val in arr){
+    console.log(val);
+}
